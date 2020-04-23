18fe0f93435dd04dc39dc3045762f1e04b18ac50
michelalejo <michelmolina65@gmail.com>
2020-04-23 18:01:11 +0000

    2-Create_DataBase.sql

--- 0x14-mysql/2-Create_DataBase.sql
+++ 0x14-mysql/2-Create_DataBase.sql
@@ -1,5 +1,6 @@
 -- Create a Database.
 CREATE DATABASE IF NOT EXISTS tyrell_corp;
 USE tyrell_corp;
-CREATE TABLE nexus6(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20));
-INSERT INTO `nexus6` (`id`,`name`) VALUES (NULL, "Leon");
+CREATE TABLE IF NOT EXISTS nexus6(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20));
+INSERT INTO  `nexus6` (`id`,`name`) VALUES (NULL, "Leon");
+GRANT SELECT on tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
