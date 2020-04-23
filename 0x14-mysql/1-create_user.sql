32b5ba599bae0e57c007ec1f3c5d25a49541ee2e
michelalejo <michelmolina65@gmail.com>
2020-04-23 12:42:17 +0000

    1-create_user.sql

--- 0x14-mysql/1-create_user.sql
+++ 0x14-mysql/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create a MySQL.
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost'
 IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT ALL PRIVILEGES ON * . * TO 'holberton_user'@'localhost';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
