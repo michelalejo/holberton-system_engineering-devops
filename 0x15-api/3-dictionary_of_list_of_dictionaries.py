9e2e41f4030f25828f9dc9c38fb3eda1c8f3df0e
michelalejo <michelmolina65@gmail.com>
2020-05-05 05:18:23 +0000

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,35 +4,39 @@
 import csv
 import json
 import requests
-from sys import argv
 
 if __name__ == "__main__":
-    id = argv[1]
-    tmp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(
-        id))
-    temp = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
+    user_info = users.json()
+    n_users = len(user_info)
+    n_users = n_users + 1
+    all_data = {}
+    for id in range(1, n_users):
+        tmp = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(id))
+        temp = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(id))
 
-    content = tmp.json()
-    info = temp.json()
-    name = content.get('name')
-    user_name = content.get('username')
-    tasks = len(info)
-    count = 0
-    for comp in info:
-        finished = comp.get('completed')
-        if finished:
-            count += 1
-    data_dic = []
-    data_list = {}
-    for task in info:
-        data_list = {
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": content.get('username')
-            }
-        data_dic.append(data_list)
-    all_data = {id: data_dic}
+        content = tmp.json()
+        info = temp.json()
+        name = content.get('name')
+        user_name = content.get('username')
+        tasks = len(info)
+        count = 0
+        for comp in info:
+            finished = comp.get('completed')
+            if finished:
+                count += 1
+        data_dic = []
+        data_list = {}
+        for task in info:
+            data_list = {
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": content.get('username')
+                }
+            data_dic.append(data_list)
+        all_data = {id: data_dic}
 
-    with open('{}.json'.format(id), 'w') as f:
+    with open('todo_all_employees.json', 'w') as f:
         json.dump(all_data, f)
