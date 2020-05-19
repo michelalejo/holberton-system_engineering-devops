189976b396465941e1a851665ee65a8fe2a28f36
michelalejo <michelmolina65@gmail.com>
2020-05-19 18:49:44 -0500

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,5 @@
-#!/usr/bin/env bash
+# With error withh sed to reemplase and remove a extra p.
+exec {'fix_with_sed':
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    provider => shell,
+}
