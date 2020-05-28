539cedfb911a291fc01d3460f2b313c3c590b621
michelalejo <michelmolina65@gmail.com>
2020-05-28 15:27:43 -0500

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,10 @@
-#!/usr/bin/env bash
+file { 'replace last line':
+    ensure  => present,
+    path    => '/etc/default/nginx',
+    content => 'ULIMIT="-n 4096"',
+}
+
+service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/default/nginx']
+}
